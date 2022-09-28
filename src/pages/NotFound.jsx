d5b993ff8b98f70127e1515b7412b8fc921a63bf
moisesfdasilva <msesfernandes@gmail.com>
2022-09-27 23:17:12 -0300

    Desenvolvida a página NotFound.jsx.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
 class NotFound extends React.Component {
   render() {
+    const linkNotFound = 'https://nerdhits.com.br/wp-content/uploads/2022/03/naruto-pergaminho-dos-selos.jpg';
     return (
-      <div>
-        <h1> Not Found Page </h1>
-      </div>
+      <main>
+        <Header />
+        <img src={ linkNotFound } alt='linkNotFound' height="300"/>
+        <h1> Página não encontrada! </h1>
+      </main>
     );
   }
 }

0f44d369a619178954b0a8d95585d12251dd0243
moisesfdasilva <msesfernandes@gmail.com>
2022-10-26 21:40:17 -0300

    Criada a página de lista alternativa.

---
+++ src/pages/AlternateCharactersList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+class AlternateCharactersList extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <main id="main-padding-top">
+          <h1>LISTA B !!!</h1>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default AlternateCharactersList;

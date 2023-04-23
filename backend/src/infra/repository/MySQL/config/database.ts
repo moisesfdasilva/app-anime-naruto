475a2f2a6d3da18e6adb76acacc6518845f081bd
moisesfdasilva <msesfernandes@gmail.com>
2023-04-23 20:28:53 -0300

    feat: desenvolvida a criação dos conteiners, nos arquivos Dockerfile da raíz do backend e frontend, no arquivo docker-compose.yml da raíz do projeto e adicionado o prestart no package.json do backend(npx -y tsc && npm run db:reset).

--- backend/src/infra/repository/MySQL/config/database.ts
+++ backend/src/infra/repository/MySQL/config/database.ts
@@ -4,8 +4,8 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
-  database: process.env.DB_NAME || 'naruto_api',
-  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'db_anime-naruto',
+  host: process.env.DB_HOST || 'db',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
 };
